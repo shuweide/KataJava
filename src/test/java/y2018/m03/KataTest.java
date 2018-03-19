24341b9402e64b3e89fad443de72f90fd81e33a2
shuweide <shuweide@gmail.com>
2018-03-19 16:27:30 +0800

    kata 2018 03 19

--- src/test/java/y2018/m03/KataTest.java
+++ src/test/java/y2018/m03/KataTest.java
@@ -8,9 +8,9 @@ public class KataTest {
 
     @Test
     public void dontGiveMeFive() {
-        assertEquals(8, Kata.dontGiveMeFive(1,9));
-        assertEquals(8, Kata.dontGiveMeFiveBest(1,9));
-        assertEquals(12, Kata.dontGiveMeFive(4,17));
-        assertEquals(12, Kata.dontGiveMeFiveBest(4,17));
+        assertEquals(8, Kata.dontGiveMeFive(1, 9));
+        assertEquals(8, Kata.dontGiveMeFiveBest(1, 9));
+        assertEquals(12, Kata.dontGiveMeFive(4, 17));
+        assertEquals(12, Kata.dontGiveMeFiveBest(4, 17));
     }
 }
