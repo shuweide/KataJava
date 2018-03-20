19b9b81df2ea0b3ccb3fd3f811d5b50e32edd896
shuweide <shuweide@gmail.com>
2018-03-20 19:47:45 +0800

    2018 03 20 kata

---
+++ src/main/java/y2018/m03/MultiplesOf3Or5.java
@@ -0,0 +1,23 @@
+package y2018.m03;
+
+import java.util.stream.IntStream;
+
+/**
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+ *
+ * Note: If the number is a multiple of both 3 and 5, only count it once.
+ */
+public class MultiplesOf3Or5 {
+    /**
+     * ```java
+     *      return IntStream.range(1, number).filter(i -> i % 3 == 0 || i % 5 == 0).sum();
+     * ```
+     * @param number
+     * @return
+     */
+    public static int solution(int number) {
+        return IntStream.range(1, number).filter(i -> i % 3 == 0 || i % 5 == 0).sum();
+    }
+}
---
+++ src/test/java/y2018/m03/MultiplesOf3Or5Test.java
@@ -0,0 +1,12 @@
+package y2018.m03;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MultiplesOf3Or5Test {
+    @Test
+    public void solution() {
+        assertEquals(23, MultiplesOf3Or5.solution(10));
+    }
+}
