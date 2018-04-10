149b1b7582ec2cadc6c71deb4034462979dcb30b
shuweide <shuweide@gmail.com>
2018-04-10 18:58:46 +0800

    add unfinish kata(Level3)

---
+++ src/main/java/y2018/unfinish/MakeSpiral.java
@@ -0,0 +1,7 @@
+package y2018.unfinish;
+
+public class MakeSpiral {
+    public static int[][] spiralize(int size) {
+        return null;
+    }
+}
---
+++ src/test/java/y2018/unfinish/MakeSpiralTest.java
@@ -0,0 +1,65 @@
+package y2018.unfinish;
+
+import org.junit.Test;
+
+import static java.util.Arrays.deepEquals;
+import static org.junit.Assert.assertTrue;
+
+public class MakeSpiralTest {
+    @Test
+    public void test1() {
+        assertTrue(deepEquals(
+                MakeSpiral.spiralize(1),
+                new int[][]{{1}}));
+    }
+
+    @Test
+    public void test2() {
+        assertTrue(deepEquals(
+                MakeSpiral.spiralize(2),
+                new int[][]{
+                        {1, 1},
+                        {0, 1}
+                }));
+    }
+
+    @Test
+    public void test3() {
+        assertTrue(deepEquals(
+                MakeSpiral.spiralize(3),
+                new int[][]{
+                        {1, 1, 1},
+                        {0, 0, 1},
+                        {1, 1, 1}
+                }));
+    }
+
+    @Test
+    public void test5() {
+        assertTrue(deepEquals(
+                MakeSpiral.spiralize(5),
+                new int[][]{
+                        {1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 1},
+                        {1, 1, 1, 0, 1},
+                        {1, 0, 0, 0, 1},
+                        {1, 1, 1, 1, 1}
+                }));
+    }
+
+    @Test
+    public void test8() {
+        assertTrue(deepEquals(
+                MakeSpiral.spiralize(8),
+                new int[][]{
+                        {1, 1, 1, 1, 1, 1, 1, 1},
+                        {0, 0, 0, 0, 0, 0, 0, 1},
+                        {1, 1, 1, 1, 1, 1, 0, 1},
+                        {1, 0, 0, 0, 0, 1, 0, 1},
+                        {1, 0, 1, 0, 0, 1, 0, 1},
+                        {1, 0, 1, 1, 1, 1, 0, 1},
+                        {1, 0, 0, 0, 0, 0, 0, 1},
+                        {1, 1, 1, 1, 1, 1, 1, 1},
+                }));
+    }
+}
