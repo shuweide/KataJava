ad6ab4679e72a4b8a30b72cc4d6ba69136f88269
shuwei.kuo <shuweide@gmail.com>
2018-04-24 21:17:34 +0800

    2018 04 24 kata refactor

--- src/test/java/y2018/m04/UserTest.java
+++ src/test/java/y2018/m04/UserTest.java
@@ -8,13 +8,28 @@ public class UserTest {
     @Test
     public void smapleTest() {
         User user = new User();
-        Assert.assertEquals(-8, user.rank());
-        Assert.assertEquals(0, user.progress());
+        Assert.assertEquals(-8, user.rank);
+        Assert.assertEquals(0, user.progress);
         user.incProgress(-7);
-        Assert.assertEquals(10, user.progress());
+        Assert.assertEquals(10, user.progress);
         user.incProgress(-5);
-        Assert.assertEquals(0, user.progress());
-        Assert.assertEquals(-7, user.rank());
+        Assert.assertEquals(0, user.progress);
+        Assert.assertEquals(-7, user.rank);
+    }
+
+    @Test
+    public void smapleAnother() {
+        User user = new User();
+        Assert.assertEquals(-8, user.rank);
+        Assert.assertEquals(0, user.progress);
+        user.incProgress(1);
+        //8*8*10= 640, -2, 40
+        Assert.assertEquals(-2, user.rank);
+        Assert.assertEquals(40, user.progress);
+        //6*6*10 = 360+40(origin),   -2 -> -1 -> 1 -> 2 -> 3, 0
+        user.incProgress(5);
+        Assert.assertEquals(3, user.rank);
+        Assert.assertEquals(0, user.progress);
     }
 
 }
